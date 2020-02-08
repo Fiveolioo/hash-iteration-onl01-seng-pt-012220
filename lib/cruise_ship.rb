72216469170a38326a49693af9fa3ea886a737ab
Richard Cho <96richardohc@gmail.com>
2020-02-08 21:38:06 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -11,7 +11,7 @@ def select_winner(passengers)
   winner = ""
   passengers.each dp |suite, name|
   if suite == :suite_a && name.start_with? ("A")
-    winnder = name
+    winner = name
   end
 end
 
